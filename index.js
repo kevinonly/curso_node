1003dc0e20ffe63d78a3765005f7716a7b6fd963
Kevin <oliveirakevin746@gmail.com>
2024-02-26 22:17:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,4 @@ require("dotenv").config();
 
 const connectToMongo = require('./src/database/connect')
 
-
 connectToMongo()
-
-//require('./modules/path')
-//require('./modules/fs')
-//require('./modules/http')
-//require('./modules/express')
-//const person = new Person('Felipe')
-
